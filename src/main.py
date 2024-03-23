88368b8384e5d77a75c5b58a8d30107480d05bab
Stefanos Chalkidis <s.chalkidis@pm.me>
2024-03-23 19:27:19 +0200

    added SigConfig

--- src/main.py
+++ src/main.py
@@ -1,2 +1,24 @@
+import sys
+from pathlib import Path
+
+import tomlkit
+from tomlkit.items import Table
+
+from esign_myphoto.config import SigConfig
+
+root_path = Path(__file__).parent.parent
+
+if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
+    root_path = Path(sys.executable).parent
+
 if __name__ == "__main__":
-    print("Hello")
+    with open(
+        root_path / "config/config.toml", mode="rt", encoding="utf-8"
+    ) as config_file:
+        toml_config = tomlkit.load(config_file)
+
+        if "signature" in toml_config and isinstance(toml_config["signature"], Table):
+            sig_config = SigConfig(toml_config["signature"])
+
+            if sig_config.license and sig_config.reason:
+                print("Hello")
