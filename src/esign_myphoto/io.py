6909faecdaeedfd944114815c63ec66245aff982
Stefanos Chalkidis <s.chalkidis@pm.me>
2024-03-24 18:21:49 +0200

    moved load_sig_config into module

---
+++ src/esign_myphoto/io.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import tomlkit
+from tomlkit.items import Table
+
+from esign_myphoto.config import SigConfig
+
+
+def load_sig_config(file_path: Path) -> SigConfig | None:
+    try:
+        with open(file_path, mode="rt", encoding="utf-8") as config_file:
+            toml_config = tomlkit.load(config_file)
+
+            if "signature" in toml_config and isinstance(
+                toml_config["signature"], Table
+            ):
+                sig_config = SigConfig(toml_config["signature"])
+
+                if sig_config.license and sig_config.reason:
+                    return sig_config
+    except FileNotFoundError:
+        print("Configuration file does not exist.")
+    return None
--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 import sys
 from pathlib import Path
 
-import tomlkit
-from tomlkit.items import Table
-
-from esign_myphoto.config import SigConfig
+from esign_myphoto import io as app_io
 
 root_path = Path(__file__).parent.parent
 
@@ -12,13 +9,4 @@ if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
     root_path = Path(sys.executable).parent
 
 if __name__ == "__main__":
-    with open(
-        root_path / "config/config.toml", mode="rt", encoding="utf-8"
-    ) as config_file:
-        toml_config = tomlkit.load(config_file)
-
-        if "signature" in toml_config and isinstance(toml_config["signature"], Table):
-            sig_config = SigConfig(toml_config["signature"])
-
-            if sig_config.license and sig_config.reason:
-                print("Hello")
+    sig_config = app_io.load_sig_config(root_path / "config/config.toml")
